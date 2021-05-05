85481091d56a2154a96ec13bfb93c8e8be837897
lso <l.schoeppe@dotsource.de>
2021-05-05 13:43:45 +0200

    notes, refresh and code/index.php added

---
+++ code/index.php
@@ -0,0 +1,15 @@
+<html>
+        <head>
+                <link rel="stylesheet" href="StyleSheet.css">
+                <title>eRead</title>
+        </head>
+        <body>
+                <?php
+					echo "hello world";
+                ?>
+                <form class=center action=index.php method="get">
+				    <input type=text id=insert name=insert><br>
+                    <input type="submit" value="Submit">
+                </form>
+        </body>
+</html>
